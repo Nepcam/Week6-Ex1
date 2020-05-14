3e574c45c6c81809266236957061f6f385572337
Nepcam <cameron.nepe@gmail.com>
2020-05-14 14:38:04 +1200

    finished

--- PracTest2/Form1.cs
+++ PracTest2/Form1.cs
@@ -22,9 +22,9 @@ namespace PracTest2
         const int DISPLAY_LEFT = 40;
         const int DISPLAY_TOP = 140;
         //the width and height of the display area of the phone
-        const int DISPLAY_WIDTH = 320; 
-        const int DISPLAY_HEIGHT = 460;
-        
+        const int DISPLAY_WIDTH = 320;
+        const int DISPLAY_HEIGHT = 600;
+
         //NOTE: If the display looks wrong in your screen resolution, 
         //please check that the picture box is 400 x 730 pixels in size
         //and adjust the form to be slightly larger than that.
@@ -59,20 +59,79 @@ namespace PracTest2
         {
             //Variables
             int numLottoBalls;
+            Random rand = new Random();
 
+            //Graphics
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            SolidBrush br = new SolidBrush(Color.Red);
+            int x = DISPLAY_LEFT;
+            int y = DISPLAY_TOP;
+            int f = 0;
 
-            //GET number of lotto balls from user input
-            numLottoBalls = int.Parse(textBoxNumLottoBalls.Text);
-            
-            //CHECK if numLottoBalls will fit the display
-            if ((BALL_SIZE + GAP_SIZE) * numLottoBalls < DISPLAY_WIDTH)
+            try
             {
-                //CALCULATE how many balls can be displayed
-                //DRAW grid of balls in various colors in smartphone display
+                //GET number of lotto balls from user input
+                numLottoBalls = int.Parse(textBoxNumLottoBalls.Text);
+                //CHECK if numLottoBalls will fit the display
+                if ((BALL_SIZE + GAP_SIZE) * numLottoBalls <= DISPLAY_WIDTH)
+                {
+
+                    for (int i = 1; i <= numLottoBalls; i++)
+                    {
+                        while ((BALL_SIZE + GAP_SIZE) + y <= DISPLAY_HEIGHT)
+                        {
+                            //CALCULATE how many balls can be displayed
+                            f = rand.Next(1, 41);
+                            if (f < 10)
+                            {
+                                br = new SolidBrush(Color.Blue);
+                            }
+                            else if (f < 20)
+                            {
+                                br = new SolidBrush(Color.Orange);
+                            }
+                            else if (f < 30)
+                            {
+                                br = new SolidBrush(Color.Green);
+                            }
+                            else if (f < 40)
+                            {
+                                br = new SolidBrush(Color.Red);
+                            }
+                            else
+                            {
+                                br = new SolidBrush(Color.Purple);
+                            }
+                            //DRAW grid of balls in various colors in smartphone display
+                            paper.FillEllipse(br, x, y, BALL_SIZE, BALL_SIZE);
+                            y += BALL_SIZE + GAP_SIZE;
+                        }
+                        y = DISPLAY_TOP;
+                        x += BALL_SIZE + GAP_SIZE;
+                    }
+                   
+                }
+
+
+
+                //IF the number is too large, no balls should be DRAWN, error message displayed
+                else
+                {
+                    //DISPLAY error message
+                    MessageBox.Show("Number will not fit in display");
+                }
+
             }
-            else
-            {
 
+            catch (Exception ex)
+            {
+                //Display error message
+                MessageBox.Show(ex.Message);
+                //Clear the textbox and set the focus
+                textBoxNumLottoBalls.Clear();
+                textBoxNumLottoBalls.Focus();
+                //Refresh the picture display
+                pictureBoxDisplay.Refresh();
             }
         }
     }
